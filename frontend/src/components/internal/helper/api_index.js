6078ff9a796ea3c0d4f042fca75940dc905af6ff
zahidDaraz <zahidh.daraz@gmail.com>
2022-10-16 11:01:41 +0600

    16-10

--- frontend/src/components/internal/helper/api_index.js
+++ frontend/src/components/internal/helper/api_index.js
@@ -11,7 +11,7 @@ const apiClient = axios.create({
 })
 
 apiClient.interceptors.request.use(request => {
-    const token =  JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
     if (token) {
         request.headers.Authorization = `Bearer ${token}`
         request.headers.AccessToken = token
